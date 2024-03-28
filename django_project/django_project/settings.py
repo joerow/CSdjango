9804948cd40563d25850cfb69048c77cbec1ac95
joerow <joerow@gmail.com>
2024-03-28 09:20:15 +0800

    add video 9

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = Path(BASE_DIR,  'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -14,10 +15,10 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email',]
+        fields = ['username', 'email']
 
 
-""" class ProfileUpdateForm(forms.ModelForm):
+class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        field = ['image'] """
+        fields = ['image'] 
